f120e598042d387d5163b70249f382c8dd98dbee
m1ke <git@m1ke.me>
2015-06-02 16:09:34 +0100

    Updated fetchAssoc and added fetchSingle

--- src/ExtendedPdo.php
+++ src/ExtendedPdo.php
@@ -19,21 +19,88 @@ class ExtendedPdo extends AuraPdo implements ExtendedPdoInterface
 		parent::__construct($dsn, $user, $pass, $options);
 	}
 
-	public function queryInsert($table_name, Array $params, Array $exclude_keys = []){
-		$query = "INSERT INTO `$table_name` ".self::makeQueryInsert($params, $exclude_keys);
-		$this->perform($query, $params);
+	/**
+	 *
+	 * Fetches an associative array of rows from the database; the rows
+	 * are returned as associative arrays, and the array of rows is keyed
+	 * on the first column of each row.
+	 *
+	 * N.b.: If multiple rows have the same first column value, the last
+	 * row with that value will override earlier rows.
+	 *
+	 * @param string $statement The SQL statement to prepare and execute.
+	 *
+	 * @param array $values Values to bind to the query.
+	 *
+	 * @param callable $callable A callable to be applied to each of the rows
+	 * to be returned.
+	 *
+	 * @param string $key_field The field to use as a key.
+	 *
+	 * @return array
+	 *
+	 */
+	public function fetchAssoc($query, array $values = [], ...$args){
+		$statement = $this->perform($query, $values);
+
+		foreach ($args as $arg){
+			if (is_callable($arg)){
+				$callable = $arg;
+			}
+			else {
+				$key_field = $arg;
+			}
+		}
+
+		$data = [];
+		while ($row = $statement->fetch(self::FETCH_ASSOC)){
+			if ($callable){
+				$row = call_user_func($callable, $row);
+			}
+			$key = $key_field ? $row[$key_field] : current($row);
+			$data[$key] = $row;
+		}
+		return $data;
+	}
+
+    /**
+     *
+     * Fetches one field from one row of the database as a scalar value.
+     *
+     * @param string $statement The SQL statement to prepare and execute.
+     *
+     * @param array $values Values to bind to the query.
+     *
+     * @return string
+     *
+     */
+	public function fetchSingle(...$args){
+		$data = $this->fetchOne(...$args);
+		$value = reset($data);
+		return $value;
+	}
+
+	public function queryInsert($table_name, Array $values, Array $exclude_keys = []){
+		$query = "INSERT INTO `$table_name` ".self::makeQueryInsert($values, $exclude_keys);
+		$this->perform($query, $values);
 		return $this->lastInsertId();
 	}
 
-	public function queryUpdate($table_name, $query, Array $params, Array $exclude_keys = []){
-		$query_update = self::makeQueryInsert($params, $exclude_keys);
-		$query = str_replace("SET :params", "SET $query_update", $query);
+	public function queryUpdate($table_name, $query, Array $values, Array $exclude_keys = []){
+		$query_update = self::makeQueryInsert($values, $exclude_keys);
+		$query = str_replace("SET :values", "SET $query_update", $query);
 		$query = "UPDATE $table_name $query";
-		return $this->fetchAffected($query, $params);
+		return $this->fetchAffected($query, $values);
 	}
 
-	public static function makeQueryParams($type, Array $params, Array $exclude_keys = []){
-		foreach ($params as $key => $val){
+	private static function makeQueryInsert(Array $values, Array $exclude_keys = []){
+		list($fields, $vals) = self::makeQueryValues('insert', $values, $exclude_keys);
+		$vals = "(".implode(',', $vals).")";
+		return "(".implode(',', $fields).") VALUES $vals";
+	}
+
+	public static function makeQueryValues($type, Array $values, Array $exclude_keys = []){
+		foreach ($values as $key => $val){
 			$value_isnt_false = $val!==false;
 			$key_has_no_dash = !in_string('-', $key);
 			$key_isnt_excluded = !in_array($key, $exclude_keys);
@@ -47,14 +114,8 @@ class ExtendedPdo extends AuraPdo implements ExtendedPdoInterface
 		return [$fields, $vals];
 	}
 
-	private static function makeQueryInsert(Array $params, Array $exclude_keys = []){
-		list($fields, $vals) = self::makeQueryParams('insert', $params, $exclude_keys);
-		$vals = "(".implode(',', $vals).")";
-		return "(".implode(',', $fields).") VALUES $vals";
-	}
-
-	private static function makeQueryUpdate(Array $params, Array $exclude_keys = []){
-		list($query) = self::makeQueryParams('update', $params, $exclude_keys);
+	private static function makeQueryUpdate(Array $values, Array $exclude_keys = []){
+		list($query) = self::makeQueryValues('update', $values, $exclude_keys);
 		return implode(',', $query);
 	}
 }
